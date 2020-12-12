95baac3f3883b831b1f596f7dfe740853e9a1591
a5365958 <2571706435@qq.com>
2020-12-12 18:34:43 +0800

    init

---
+++ src/main/kotlin/com/zhaoping/hr/wxjobs/WxjobsApplication.kt
@@ -0,0 +1,11 @@
+package com.zhaoping.hr.wxjobs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WxjobsApplication
+
+fun main(args: Array<String>) {
+	runApplication<WxjobsApplication>(*args)
+}
---
+++ src/test/kotlin/com/zhaoping/hr/wxjobs/WxjobsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zhaoping.hr.wxjobs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WxjobsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
